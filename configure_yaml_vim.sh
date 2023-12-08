ef2903b93a77b669d7b030f24c626a20964340a7
Du2s <onesecondch@gmail.com>
2023-12-08 19:11:46 +0000

    Changed SED to GREP

--- configure_yaml_vim.sh
+++ configure_yaml_vim.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-sed -i '/filetype plugin indent on/d' /home/$USER/.vimrc
+grep -qF "filetype plugin indent on" /home/$USER/.vimrc || \
 echo "filetype plugin indent on" >> /home/$USER/.vimrc
 
-sed -i '/autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab/d' /home/$USER/.vimrc
+grep -qF "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" /home/$USER/.vimrc || \
 echo "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" >> /home/$USER/.vimrc
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-sed -i "/execute pathogen#infect()/d" /home/$USER/.vimrc
+grep -qF "execute pathogen#infect()" /home/$USER/.vimrc || \
 echo "execute pathogen#infect()" >> /home/$USER/.vimrc
 
 cd ~/.vim/bundle && \
